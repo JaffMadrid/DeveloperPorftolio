49acebe615760b7985dec2233d84ee0835ab5f55
JaffMadrid <jafetmadrid4794@gmail.com>
2025-10-02 08:16:58 -0600

    Fixing experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,13 +1,13 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Lead Application Developer',
-    company: "Startup Founder",
+    title: 'Lead IOS/Android React Native Application Lead Developer',
+    company: "Startup Founder/ Self Employed",
     duration: "(Mar 2025 - Present)"
   },
   {
     id: 2,
-    title: "FullStack Web Developer",
+    title: "FullStack React Web Developer",
     company: "Arca de Esperanzas",
     duration: "(Aug 2024 - Mar 2025)"
   },
