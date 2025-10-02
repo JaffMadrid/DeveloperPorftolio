a5ddfa3efe2659231e8d9c9fec859c13ae8ec3e1
JaffMadrid <jafetmadrid4794@gmail.com>
2025-10-02 08:15:35 -0600

    Adding education

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import dynamic from 'next/dynamic';
 const HeroSection = dynamic(() => import("./components/homepage/hero-section"), { ssr: false });
 const AboutSection = dynamic(() => import("./components/homepage/about"), { ssr: false });
 const Experience = dynamic(() => import("./components/homepage/experience"), { ssr: false });
+const Education = dynamic(() => import("./components/homepage/education"), { ssr: false });
 const Skills = dynamic(() => import("./components/homepage/skills"), { ssr: false });
 const Projects = dynamic(() => import("./components/homepage/projects"), { ssr: false });
 const ContactSection = dynamic(() => import("./components/homepage/contact"), { ssr: false });
@@ -15,6 +16,7 @@ export default function Home() {
     <>
       <HeroSection />
       <AboutSection />
+      <Education />
       <Experience />
       <Skills />
       <Projects />
