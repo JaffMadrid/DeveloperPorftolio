ec431919a05596c04cdca765bf50ce6cce0424dd
JaffMadrid <jafetmadrid4794@gmail.com>
2025-10-02 08:17:28 -0600

    Education fix

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,7 +1,7 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree in Software Engineering",
+    title: "Bachelor's Degree in Software Engineering",
     duration: "2020 - 2025",
     institution: "Universidad Tecnológica Centroamericana (CEUTEC)",
   },
