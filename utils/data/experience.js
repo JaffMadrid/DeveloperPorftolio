ad1d5ee5bbabdebec5fe134e661c0686e1eeb201
JaffMadrid <jafetmadrid4794@gmail.com>
2025-10-10 00:19:53 -0600

    updating experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,14 +1,20 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Lead IOS/Android React Native Application Lead Developer',
-    company: "Startup Founder/ Self Employed",
-    duration: "(Mar 2025 - Present)"
+    title: 'Lead IOS/Android React Native Lead Developer',
+    company: "Grupo NOMA",
+    duration: "(Jan 2025 - Present)"
   },
   {
     id: 2,
     title: "FullStack React Web Developer",
     company: "Arca de Esperanzas",
-    duration: "(Aug 2024 - Mar 2025)"
+    duration: "(Aug 2024 - Jan 2025)"
+  },
+   {
+    id: 3,
+    title: "React Developer",
+    company: "Pittsburgh Knights",
+    duration: "(Oct 2022 - Aug 2024)"
   },
 ]
