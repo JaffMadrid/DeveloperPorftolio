eaf571a6fa995c0c2648a8062073fd2e07bbc6c7
JaffMadrid <jafetmadrid4794@gmail.com>
2025-10-10 00:19:43 -0600

    Updatating experience

--- app/page.js
+++ app/page.js
@@ -16,8 +16,8 @@ export default function Home() {
     <>
       <HeroSection />
       <AboutSection />
-      <Education />
       <Experience />
+      <Education />
       <Skills />
       <Projects />
       <ContactSection />
